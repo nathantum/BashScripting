8648b4b577b0c27492fd2a9b17d28cd6eaecda66
Nathan Tum <tumkipsang@gmail.com>
2025-03-17 09:16:09 -0500

    first commit

---
+++ chap1-exercises.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Write a shell script in that prints your user name.
+echo "Who are you?"
+
+# Write a shell script in that prints your current working directory.
+echo "$PWD"
+
+# Write a shell script in that prints the absolute path to your home directory
+echo "$HOME"
+
+# Write a shell script in that prints the default shell.
+echo "$SHELL"
